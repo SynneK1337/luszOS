4450b32fb580a7ca14fde7d72cd1920ff3ed703f
SynneK1337 <synnek1337@protonmail.com>
2020-01-26 13:50:32 +0100

    terminal: make code cleaner, fix all compiler warnings

--- kernel/terminal/terminal.cc
+++ kernel/terminal/terminal.cc
@@ -8,26 +8,34 @@ size_t terminal::row = 0;
 uint16_t* const terminal::buffer = (uint16_t*) 0xB8000;
 
 
-void terminal::putchar(char c, uint8_t color = terminal::color, size_t x = terminal::column, size_t y = terminal::row) {
+void terminal::putchar(char c, uint8_t color, size_t x, size_t y) {
     const size_t offset = vga::MAX_WIDTH * y + x;
     terminal::buffer[offset] = vga::get_char_entry(c, color);
     ++terminal::column;
     if (terminal::column > vga::MAX_WIDTH) {
-        ++terminal::row;
-        terminal::column = 0;
+        terminal::newline();
     }
 }
 
 void terminal::clear() {
     auto color = vga::get_color(vga::LIGHT_GREY, vga::BLACK);
-    for (auto offset = 0; offset < vga::MAX_WIDTH * vga::MAX_HEIGHT; ++offset) {
+    for (size_t offset = 0; offset < vga::MAX_WIDTH * vga::MAX_HEIGHT; ++offset) {
         terminal::buffer[offset] = vga::get_char_entry(' ', color);
     }
 }
 
-void terminal::putstr(const char str[], uint8_t color = terminal::color, size_t x = terminal::column, size_t y = terminal::row) {
-    size_t len = strlen(str);
-    for (auto i = 0; i < len; ++i) {
-        terminal::putchar(str[i]);
-    }
+void terminal::newline() {
+    ++terminal::row;
+    terminal::column = 0;
+}
+
+void terminal::putstr(const char str[], uint8_t color, size_t x, size_t y) {
+    auto len = strlen(str);
+    for (size_t i = 0; i < len; ++i) {
+        terminal::putchar(str[i], color, x, y);
+        ++x;
+        if (x > vga::MAX_WIDTH) {
+            terminal::newline();
+        }
+    }   
 }
--- kernel/terminal/terminal.hxx
+++ kernel/terminal/terminal.hxx
@@ -9,6 +9,7 @@ namespace terminal {
     extern size_t column, row;
     extern uint16_t* const buffer;
     void clear();
+    void newline();
     void putchar(char c, uint8_t color = terminal::color, size_t x = terminal::column, size_t y = terminal::row);
     void putstr(const char str[], uint8_t color = terminal::color, size_t x = terminal::column, size_t y = terminal::row);
 }
